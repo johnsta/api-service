472c5dfa807402cd3c6ffaad55319236e938b5fd
John Stallo <john.stallo@live.com>
2018-11-15 16:51:51 -0800

    basic API handler

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 var express = require("express");
 var app = express();
 
+app.get('/', function (req, res) {
+    res.status(200).send('Hello from api-service\n');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Server running on port " + port);
+});
+
+process.on("SIGINT", () => {
+    process.exit(130 /* 128 + SIGINT */);
+});
+
+process.on("SIGTERM", () => {
+    console.log("Terminating...");
+    server.close();
 });
